f5e84615063637f433901aec00d357f355536905
belo355 <edilson.belo@live.com>
2020-10-01 01:49:28 -0300

    add components

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      width: 100%;
+      color: #F4EDE8;
+
+      &::placeholder {
+        color: #666360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+
+
+`;
--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,33 +1,34 @@
 import React from 'react';
-import {FiLogIn} from 'react-icons/fi';
+import { FiLogIn } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
 
-import {Container, Content, Background} from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
-<Container>
-  <Content>
-  <img src={logo} alt="goBarber"/>
+  <Container>
+    <Content>
+      <img src={logo} alt="goBarber" />
 
-  <form>
-      <h1> Faça seu login</h1>
-      <input  type="email" placeholder="Email"/>
-      <input type="password "placeholder="Senha"/>
-      <button type="submit"> Entrar </button>
+      <form>
+        <h1> Faça seu login</h1>
+        <Input name="email" type="email" placeholder="Email" />
+        <Input name="password" type="password " placeholder="Senha" />
+        <Button type="submit"> Entrar </Button>
 
-      <a href="forgot"> Esqueci minha senha</a>
+        <a href="forgot"> Esqueci minha senha</a>
+      </form>
 
-  </form>
-
-  <a href="">
-  <FiLogIn />
-  Criar conta
-  </a>
-
-  </Content>
-  <Background />
-</Container>
+      <a href="">
+        <FiLogIn />
+        Criar conta
+      </a>
+    </Content>
+    <Background />
+  </Container>
 );
 
 export default SignIn;
